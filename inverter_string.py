6b21e095919012ebef2b21534e43fadc2b385e1d
Luciano <55037889+lucenfort@users.noreply.github.com>
2023-03-07 15:59:17 -0300

    Update inverter_string.py

--- inverter_string.py
+++ inverter_string.py
@@ -1,1 +1,9 @@
+def inverter_string(texto):
+    texto_invertido = ""
+    for i in range(len(texto) - 1, -1, -1):
+        texto_invertido += texto[i]
+    return texto_invertido
 
+texto = "Exemplo de string a ser invertida"
+texto_invertido = inverter_string(texto)
+print(texto_invertido)
