323f3c1f8fea683339acd427643359fba3469321
Luciano <55037889+lucenfort@users.noreply.github.com>
2023-03-07 15:56:14 -0300

    Update soma.py

--- soma.py
+++ soma.py
@@ -1,1 +1,17 @@
 
+def somar_ate(n: int) -> int:
+    """Calcula a soma de todos os números inteiros de 1 a n.
+
+    Args:
+        n (int): O valor limite para a soma.
+
+    Returns:
+        int: A soma de todos os números inteiros de 1 a n.
+    """
+    soma = 0
+    for i in range(1, n+1):
+        soma += i
+    return soma
+
+# Exemplo:
+print(somar_ate(100))
