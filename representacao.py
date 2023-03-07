7755bcaadab5df49a6500fded331bc148ad37d6d
Luciano <55037889+lucenfort@users.noreply.github.com>
2023-03-07 15:59:00 -0300

    Update representacao.py

--- representacao.py
+++ representacao.py
@@ -1,1 +1,17 @@
+faturamento = {
+    "SP": 67836.43,
+    "RJ": 36678.66,
+    "MG": 29229.88,
+    "ES": 27165.48,
+    "Outros": 19849.53
+}
 
+# Calcula o faturamento total
+total_faturamento = sum(faturamento.values())
+
+# Calcula o percentual de representação de cada estado
+percentuais = {estado: (valor / total_faturamento) * 100 for estado, valor in faturamento.items()}
+
+# Imprime os resultados
+for estado, percentual in percentuais.items():
+    print(f"{estado}: {percentual:.2f}%")
